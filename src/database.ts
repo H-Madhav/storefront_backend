95f1dec786dd244f94e6275c1a95b32aa7011324
hari.mj <hari.mj@incedoinc.com>
2021-04-30 18:01:53 +0530

    add database file

---
+++ src/database.ts
@@ -0,0 +1,37 @@
+const dotenv = require('dotenv');
+import { Pool } from 'pg';
+
+
+dotenv.config()
+const {
+    POSTGRES_HOST,
+    POSTGRES_DB,
+    POSTGRES_TEST_DB,
+    POSTGRES_USER,
+    POSTGRES_PASSWORD,
+    ENV
+} = process.env 
+
+let client = new Pool();
+
+if(ENV==='test') {
+    client = new Pool({
+        host: POSTGRES_HOST,
+        database: POSTGRES_TEST_DB,
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD,
+    })
+}
+
+
+if(ENV==='dev') {
+    client = new Pool({
+        host: POSTGRES_HOST,
+        database: POSTGRES_DB,
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD,
+    })
+}
+
+
+export default client
