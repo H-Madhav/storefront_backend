7cdae02743a335352c1b0f15275fdd55754df24d
hari.mj <hari.mj@incedoinc.com>
2021-05-01 08:46:26 +0530

    add products model and handlers

---
+++ src/handlers/products.ts
@@ -0,0 +1,47 @@
+import { Request, Response, Application } from 'express';
+import { Product, ProductStore } from '../models/products'
+import verifyAuthToken from '../middleware/verifyAuthToken';
+
+const store = new ProductStore()
+
+const index = async (_req: Request, res: Response) => {
+    try {
+        const products = await store.index()
+        res.json(products)
+    } catch(err) {
+        res.status(404)
+        res.json(err)
+    }
+}
+
+const show = async (req: Request, res: Response) => {
+    try {
+        const product = await store.show(req.params.id)
+        res.json(product)
+    } catch(err) {
+        res.status(404)
+        res.json(err)
+    }
+}
+
+const create = async (req: Request, res: Response) => {
+    try {
+        const product: Product = {
+            name: req.body.name,
+            price: req.body.price
+        }
+        const newProduct = await store.create(product)
+        res.json(newProduct)
+    } catch(err) {
+        res.status(400)
+        res.json(err)
+    }
+}
+
+const productRoutes = (app: Application) => {
+  app.get('/products', verifyAuthToken, index)
+  app.get('/products/:id', verifyAuthToken, show)
+  app.post('/products', verifyAuthToken, create)
+}
+
+export default productRoutes;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
 import userRoutes from './handlers/users';
+import productRoutes from './handlers/products';
 
 const app: express.Application = express()
 const address: string = "0.0.0.0:3000"
@@ -11,6 +12,7 @@ app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!')
 })
 userRoutes(app);
+productRoutes(app);
 
 app.listen(3000, function () {
     console.log(`starting app on: ${address}`)
